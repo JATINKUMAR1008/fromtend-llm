863daac381b0882a0ebfd54c3d468a3dffcf630e
Jatin <jatinkumar10082003@gmail.com>
2024-04-07 01:23:29 +0530

    revert back

--- src/pages/api/stream.ts
+++ src/pages/api/stream.ts
@@ -1,37 +1,41 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
- 
+import { NextRequest } from 'next/server';
+
 export const runtime = 'edge'
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
-    const body = await req.body
+    const body = await req.json()
     console.log(body)
-    res.write('stream start')
     const response = await fetch(`${process.env.NEXT_PUBLIC_API}/ai_response/${body.chatId}`, {
-        method: 'POST',
-        body: JSON.stringify({ input_str: body.input }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
+      method: 'POST',
+      body: JSON.stringify({ input_str: body.input }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
     });
-    const reader = response.body ? response.body.getReader() : null;
-    const decoder = new TextDecoder();
-        
-    if (reader) {
-            let chunk = await reader.read();
-            while (!chunk.done) {
-                let resChunk = decoder.decode(chunk.value, { stream: true });
-                //@ts-ignore
-                res.write(resChunk)
-                chunk = await reader.read();
-            }
-            res.end()
+    const stream = new ReadableStream({
+      async start(controller) {
+        const reader = response.body ? response.body.getReader() : null;
+        const decoder = new TextDecoder();
+
+        if (reader) {
+          let chunk = await reader.read();
+          while (!chunk.done) {
+            let resChunk = decoder.decode(chunk.value, { stream: true });
+            //@ts-ignore
+            controller.enqueue(resChunk)
+            chunk = await reader.read();
+          }
         }
-    else{
-        res.end()
-    }
-  
+        controller.close()
+
+      }
+
+    })
+    return new Response(stream)
+
   } else {
     // Handle any other HTTP method
-    res.status(405).json({error: "Method not allowed"})
+    res.status(405).json({ error: "Method not allowed" })
   }
 }
