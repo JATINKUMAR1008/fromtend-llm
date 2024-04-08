c0b135b7108c8dc70163e30742ca11db1c35a854
Jatin <jatinkumar10082003@gmail.com>
2024-04-08 18:31:50 +0530

    revert changes back

--- src/app/chat/[slug]/page.tsx
+++ src/app/chat/[slug]/page.tsx
@@ -80,13 +80,13 @@ function Chat({ params, refetch }: IChatComponents) {
             content: 'thinking...'
         }] as IMessage[]);
 
-        // const res = await fetch(`${process.env.NEXT_PUBLIC_API}/ai_response/${chatId}`, {
-        //     method: 'POST',
-        //     body: JSON.stringify({ input_str: input }),
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     }
-        // });
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API}/ai_response/${chatId}`, {
+            method: 'POST',
+            body: JSON.stringify({ input_str: input }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
         // const res = await fetch(`/api/stream`, {
         //     method: 'POST',
         //     body: JSON.stringify({ input, chatId }),
