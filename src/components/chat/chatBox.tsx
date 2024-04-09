abe679624539b8c509b7323fae88cebac505b707
Jatin <jatinkumar10082003@gmail.com>
2024-04-09 14:59:25 +0530

    scrollbar formatting

--- src/components/chat/chatBox.tsx
+++ src/components/chat/chatBox.tsx
@@ -16,7 +16,7 @@ import remarkGfm from 'remark-gfm'
 export const ChatBox = ({ message }: Message) => {
 
     return message.sent_from === 'ai' ? (
-        <div className="w-full xl:px-56 px-10 py-2 ">
+        <div className="w-full xl:px-56 px-10 py-2 scrollbar-hidden">
             <div className="flex gap-2 items-start">
                 <Image src={ai_img} alt="ai" width={30} height={30} />
                 <div className=" p-2 rounded-md markdown_comp">
@@ -25,7 +25,7 @@ export const ChatBox = ({ message }: Message) => {
             </div>
         </div>
     ) : (
-        <div className="w-full xl:px-56 py-2 px-10 ">
+        <div className="w-full xl:px-56 py-2 px-10 scrollbar-hidden">
             <div className="flex gap-2 items-start">
                 <Image src={user_img} alt="user" width={30} height={30} />
                 <div className="p-2 rounded-md">
