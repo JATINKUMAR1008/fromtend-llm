1ee9e32bef16e63130dc038764273f31f87241a3
jatin <jatinkumar10082003@gmail.com>
2024-04-10 19:12:10 +0530

    fix:some height issues

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
     const router = useRouter()
     const dispatch = useDispatch()
     return (
-        <div className="w-full fixed z-10 flex py-4 items-center justify-between border-b-[1px] border-neutral-700   px-10">
+        <div className="w-full fixed z-10 h-14 flex py-4 items-center justify-between border-b-[1px] border-neutral-700 bg-[#202026]  px-10">
             <div className="cursor-pointer md:hidden" onClick={() => dispatch(changeSidebarState())}><RiMenu2Fill size={20} /></div>
             <h1 className="text-lg font-semibold font-sans">GAIA<span className="ml-1 italic font-sans">(testing)</span></h1>
             <div className="cursor-pointer" onClick={() => router.push('/')}><IoCreateOutline size={25} /></div>
